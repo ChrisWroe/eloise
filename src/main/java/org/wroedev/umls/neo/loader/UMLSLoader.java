2bb6590f3df3f59821b12202e039d4a15e18b921
Chris Wroe <chris.j.wroe@gmail.com>
2011-09-22 17:23:13 +0100

    now working on batch insert and index

--- src/main/java/org/wroedev/umls/neo/loader/UMLSLoader.java
+++ src/main/java/org/wroedev/umls/neo/loader/UMLSLoader.java
@@ -8,12 +8,39 @@ import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
+import org.neo4j.kernel.impl.batchinsert.BatchInserter;
 
 public class UMLSLoader {
 	
-	public String[] getColumnNamesForFile(String filename, String definitionFilePath) throws FileNotFoundException, IOException {
+	private String mrColsFilePath,mrFilesFilePath,mrIndexFilePath;
+	
+	public String getMrIndexFilePath() {
+		return mrIndexFilePath;
+	}
+
+	public void setMrIndexFilePath(String mrIndexFilePath) {
+		this.mrIndexFilePath = mrIndexFilePath;
+	}
+
+	public String getMrColsFilePath() {
+		return mrColsFilePath;
+	}
+
+	public void setMrColsFilePath(String mrColsFilePath) {
+		this.mrColsFilePath = mrColsFilePath;
+	}
+
+	public String getMrFilesFilePath() {
+		return mrFilesFilePath;
+	}
+
+	public void setMrFilesFilePath(String mrFilesFilePath) {
+		this.mrFilesFilePath = mrFilesFilePath;
+	}
+
+	public String[] getColumnNamesForFile(String filename) throws FileNotFoundException, IOException {
 		
-		LineIterator iterator = IOUtils.lineIterator(new FileInputStream(definitionFilePath), "UTF-8");
+		LineIterator iterator = IOUtils.lineIterator(new FileInputStream(mrFilesFilePath), "UTF-8");
 		
 		
 		String[] fields;
@@ -30,9 +57,8 @@ public class UMLSLoader {
 		return cols.split(",");
 	}
 
-	public String getColumnTypeForFile(String columnName, String fileName,
-			String mrcolsFilePath) throws FileNotFoundException, IOException {
-LineIterator iterator = IOUtils.lineIterator(new FileInputStream(mrcolsFilePath), "UTF-8");
+	public String getColumnTypeForFile(String columnName, String fileName) throws FileNotFoundException, IOException {
+LineIterator iterator = IOUtils.lineIterator(new FileInputStream(mrColsFilePath), "UTF-8");
 		
 		
 		String[] fields;
@@ -49,12 +75,30 @@ LineIterator iterator = IOUtils.lineIterator(new FileInputStream(mrcolsFilePath)
 		return colType;
 	}
 	
-	public String[][] getPropertyTypesForFile(String fileName,String mrColsPath,String mrFilesPath) throws FileNotFoundException, IOException {
+	public String getIndexTypeForFile(String columnName, String fileName) throws FileNotFoundException, IOException {
+		LineIterator iterator = IOUtils.lineIterator(new FileInputStream(mrIndexFilePath), "UTF-8");
+				
+				
+				String[] fields;
+				String indexType= "";
+				while (iterator.hasNext()) {
+				     String line = iterator.nextLine();
+				     fields = line.split("\\|");
+				     
+				     if (fields[0].equals(columnName) && fields[1].equals(fileName)) {
+				    	 indexType = fields[2];
+				     }
+				     
+				   }
+				return indexType;
+			}
+	
+	public String[][] getPropertyTypesForFile(String fileName) throws FileNotFoundException, IOException {
 		
-		String[] cols = getColumnNamesForFile(fileName, mrFilesPath);
+		String[] cols = getColumnNamesForFile(fileName);
 		String[] colTypes = new String[cols.length];
 		for (int i = 0; i < cols.length; i++) {
-			colTypes[i]=getColumnTypeForFile(cols[i], fileName, mrColsPath);
+			colTypes[i]=getColumnTypeForFile(cols[i], fileName);
 		}
 		
 		String[][] result = {cols,colTypes};
@@ -62,5 +106,40 @@ LineIterator iterator = IOUtils.lineIterator(new FileInputStream(mrcolsFilePath)
 		return result;
 		
 	}
+	
+	
+	
+	public LineIterator fileIterator(String filePath) throws FileNotFoundException, IOException {
+		return IOUtils.lineIterator(new FileInputStream(filePath), "UTF-8");
+	}
+	
+	private Map<String,Object> buildPropertyMap(String[] fields,String[][] def) {
+		Map<String,Object> properties = new HashMap<String,Object>();
+		for (int i = 0; i < fields.length; i++) {
+			String property = fields[i];
+			String propertyName = def[0][i];
+			String propertyType = def[1][i];
+			properties.put(propertyName, property);
+		}
+		return properties;
+	}
 
+	public void loadFile(String filename,String filepath, BatchInserter inserter) throws FileNotFoundException, IOException {
+		String[][] def = getPropertyTypesForFile(filename);
+		LineIterator it = fileIterator(filepath);
+		String[] fields;
+		int count = 0;
+		
+		while(count<100 & it.hasNext()) {
+			String line = it.nextLine();
+		    fields = line.split("\\|");
+		    Map<String, Object> properties = buildPropertyMap(fields,def);
+		    inserter.createNode(properties);
+		    
+		    
+		    count++;
+		}
+				
+	}
+	
 }
--- src/test/java/org/wroedev/umls/neo/loader/UMLSLoaderTest.java
+++ src/test/java/org/wroedev/umls/neo/loader/UMLSLoaderTest.java
@@ -2,13 +2,18 @@ package org.wroedev.umls.neo.loader;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.neo4j.kernel.impl.batchinsert.BatchInserter;
+import org.neo4j.kernel.impl.batchinsert.BatchInserterImpl;
 
 public class UMLSLoaderTest {
 	private static String mrconsoFilePath;
@@ -17,6 +22,8 @@ public class UMLSLoaderTest {
 	
 	private static String mrconso="MRCONSO.RRF";
 	private static String mrcolsFilePath;
+	private UMLSLoader loader;
+	private static String mrindexFilePath;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -26,13 +33,18 @@ public class UMLSLoaderTest {
 		mrconsoFilePath = basePath+"MRCONSO.RRF";
 		mrfilesFilePath = basePath+"MRFILES.RRF";
 		mrcolsFilePath = basePath+"MRCOLS.RRF";
+		mrindexFilePath = "MRINDEX.ME";
 	}
 
-	private UMLSLoader loader;
+	
 
 	@Before
 	public void setUp() throws Exception {
+		FileUtils.deleteDirectory(new File("neo4j-db/"));
 		loader = new UMLSLoader();
+		loader.setMrColsFilePath(mrcolsFilePath);
+		loader.setMrFilesFilePath(mrfilesFilePath);
+		loader.setMrIndexFilePath(mrindexFilePath);
 	}
 
 	@After
@@ -41,21 +53,35 @@ public class UMLSLoaderTest {
 
 	@Test
 	public void testReadColumnDefinition() throws Exception {
-		String[] cols = loader.getColumnNamesForFile(mrconso, mrfilesFilePath);
+		String[] cols = loader.getColumnNamesForFile(mrconso);
 		assertEquals(18,cols.length);
 	}
 	
 	@Test
 	public void testReadColumnType() throws Exception {
-		String colType = loader.getColumnTypeForFile("CUI",mrconso, mrcolsFilePath);
+		String colType = loader.getColumnTypeForFile("CUI",mrconso);
 		assertEquals("char(8)",colType);
 	}
 	
 	@Test
 	public void testGetColumnInfo() throws Exception {
-		String[][] colTypes = loader.getPropertyTypesForFile(mrconso, mrcolsFilePath, mrfilesFilePath);
+		String[][] colTypes = loader.getPropertyTypesForFile(mrconso);
 		assertEquals("CUI",colTypes[0][0]);
 		assertEquals("char(8)",colTypes[1][0]);
+	}
+	
+	@Test
+	public void testGetIndexInfo() throws Exception {
+		String indexType = loader.getIndexTypeForFile("CUI",mrconso);
+		assertEquals("Exact",indexType);
+		
+	}
+	
+	@Test
+	public void testLoadFiles() throws Exception {
+		BatchInserter inserter = new BatchInserterImpl( "neo4j-db/" );
+		loader.loadFile(mrconso, mrconsoFilePath,inserter);
+		inserter.shutdown();
 	}
 
 }
